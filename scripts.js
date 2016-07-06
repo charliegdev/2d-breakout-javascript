9955c4cb2f4df037955cc273d818ac4d0bf26eb0
Charlie Guan <charliegdev@gmail.com>
2016-07-05 21:26:59 -0700

    Draw a moving ball

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
-var canvas = document.getElementById("game-region");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById("game-region"),
+    ctx = canvas.getContext("2d"),
+    x = canvas.width/2,
+    y = canvas.height-30,
+    dx = 1,
+    dy = -1;
 
-function draw() {
+function drawBall() {
     "use strict";
     ctx.beginPath();
-    ctx.arc(50, 50, 20, 0, Math.PI*2);
+    ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
-window.setInterval(draw, 10);
+
+function draw() {
+    "use strict";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+window.setInterval(draw, 5);
 
